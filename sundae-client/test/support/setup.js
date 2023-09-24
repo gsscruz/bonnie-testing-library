c257ab8567caae4143c771a4d66c0bf4d09ba71e
Gustavo Monte <gmonte@tutanota.com>
2023-09-23 22:09:44 -0300

    tests: add msw

--- sundae-client/test/support/setup.js
+++ sundae-client/test/support/setup.js
@@ -1,1 +1,7 @@
 import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+import { beforeAll, afterAll, afterEach } from "vitest";
+
+beforeAll(() => server.listen({ onUnhandleRequest: "error" }));
+afterAll(() => server.close());
+afterEach(() => server.resetHandlers());
